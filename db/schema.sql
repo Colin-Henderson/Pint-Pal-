a724ef31c2fe435d4f7d6479eea068a04d7c4dd1
Colin Henderson <colinhenderson@Colins-MacBook-Pro.local>
2018-02-17 13:53:07 -0500

    added sql seeds

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+-- state , style , abv , name ,  etc.
+CREATE DATABASE pintpal_db;
+
+USE pintpal_db;
+
+CREATE TABLE beerlog
+(
+	id int NOT NULL AUTO_INCREMENT,
+	brand_name varchar(255) NOT NULL,
+	beer_name varchar(255) NOT NULL, 
+	state_abbrev varchar(30),
+	beer_style varchar(40),
+	abv Decimal (4,2), 
+	rating integer(10) NOT NULL, 
+	notes varchar(350), 
+	PRIMARY KEY (id)
+);
+
+-- CREATE TABLE masterbeer (
+-- )
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 // Need to put this in whatever file we 
 // are connection to our database in 
 const db = require('db')
-db.connect({
+  db.connect({
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS
