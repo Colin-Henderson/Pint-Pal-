4face2ea1f4cd9a8b80f53eb4f409b4362653a3d
Colin Henderson <colinhenderson@Colins-MacBook-Pro.local>
2018-02-17 12:27:30 -0500

    added to gitignore

---
+++ server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+// Need to put this in whatever file we 
+// are connection to our database in 
+const db = require('db')
+db.connect({
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS
+})
