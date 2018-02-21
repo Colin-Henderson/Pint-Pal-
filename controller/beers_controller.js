255b690eb6089f3a91162f72cfb5e3a3ded45bf7
jok405 <john.y.kim84@gmail.com>
2018-02-20 23:39:53 -0500

    controllers added

---
+++ controller/beers_controller.js
@@ -0,0 +1,35 @@
+// Node Dependencies
+const express = require('express');
+const router = express.Router();
+const beer = require('../models/beer.js');
+
+// Index Redirect
+router.get('/', function (req, res) {
+  res.redirect('/index');
+});
+
+// Index Page (render all beers to DOM)
+router.get('/index', function (req, res) {
+  beer.selectAll(function(data) {
+    var hbsObject = { beers: data };
+    //console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+
+// Create a New Beer
+router.post('/beer/create', function (req, res) {
+  beer.insertOne(req.body.beer_name, function() {
+    res.redirect('/index');
+  });
+});
+
+// Drink a Beer
+router.post('/beer/drink/:id', function (req, res) {
+  beer.updateOne(req.params.id, function() {
+    res.redirect('/index');
+  });
+});
+
+// Export routes
+module.exports = router;
