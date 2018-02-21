79d0d5d8fcf48e272c83e519142e889228dd7fb0
jok405 <john.y.kim84@gmail.com>
2018-02-20 23:33:33 -0500

    server.js update

--- server.js
+++ server.js
@@ -8,3 +8,25 @@ const db = require('db')
   username: process.env.DB_USER,
   password: process.env.DB_PASS
 })
+
+/*         John's addition                   */
+const express = require('express');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const router = require('./controllers/beers_controller.js');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Manages app's static content from the "public" directory within application directory.
+app.use(express.static(process.cwd() + '/public')); 
+app.use(bodyParser.urlencoded({ extended: false }));        // Parse app | proper url encode
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use('/', router); 
+
+
+app.listen(port, function() {
+    console.log("App listening on PORT " + port);  // status 200 check
+});
