f0db435ca909707321ee0be778f8e3c310c4f0ad
Colin Henderson <colinhenderson@Colins-MacBook-Pro.local>
2018-02-27 20:47:24 -0500

    got the api endpoint working on local host. now can use its features

--- brewerdb/public/app.js
+++ brewerdb/public/app.js
@@ -1,14 +1,9 @@
-// var authKey = "609ac568fb188f92b20356f14f8cc008";
-
-// // queryURLBase is the start of our API endpoint. 
-// // the user hits the search button
-// var queryURLBase = "https://api.brewerydb.com/v2/?key=" + authKey;
-
-  // http://api.brewerydb.com/v2/?key=abcdef
-
-
-// need to do a feth to local server and then that is going to fetch to brewery db 
-
+// need to do a fetch to local server and then that is going to fetch to brewery db 
+// set up an endpoint on my server 
+// send a request to my server to the endpoint in step one 
+// that endpoint is going to make a request to brewery Db
+// when the server gets a response ffrom brewery db it will respond to the client 
+// going to be asynchronyous going to be like mysql queries no res. or res. until brewery db has responded 
 
 
 $(function() {
@@ -21,21 +16,14 @@ $(function() {
 		    url: "/brewerydb",
 		    method: "GET"
 	  	}).done(function(beerData) {
-	  		console.log("testing")
+	  		console.log(beerData)
+	  		$("#brewery").html(beerData)
+	  		// Look back at omdb HW data and use a for loop to get through the array and then do something with it 
 	  		// console.log("")
 	  })
-
 	};
   runQuery();  
 });
-// runQuery();  
-// look at jquerys examples
-// .Get/Post
 
 
-// set up an endpoint on my server 
-// send a request to my server to the endpoint in step one 
-// that endpoint is going to make a request to brewery Db
-// when the server gets a response ffrom brewery db it will respond to the client 
 
-// going to be asynchronyous going to be like mysql queries no res. or res. until brewery db has responded 
--- brewerdb/server.js
+++ brewerdb/server.js
@@ -5,13 +5,22 @@ var path = require("path");
 var app = express();
 
 var port = process.env.PORT || 3000;
+var key = '609ac568fb188f92b20356f14f8cc008';
 
 
 app.get('/brewerydb', function (req, res) {
-    fetch('https://api.brewerydb.com/v2/?key=609ac568fb188f92b20356f14f8cc008')
-        .then(data => {
-            res.send(data);
-        });
+	console.log("test")
+    fetch('https://api.brewerydb.com/v2/search?key=' + key + '&q=Gose&type=beer')
+	.then(data => data.json())
+	.then(body => res.send(body));
+});
+
+app.get('/geo/:lat/:lng', function (req, res) {
+	console.log(req.params.lat)
+	console.log(req.params.lng)
+    fetch('https://api.brewerydb.com/v2/search?key=' + key + '&q=geo/point?lat=40.5407&lng=40.4360')
+	.then(data => data.json())
+	.then(body => res.send(body));
 });
 
 // app.use(express.static("public"));
@@ -28,3 +37,4 @@ app.listen(port, function() {
 });
 
 console.log(__dirname);
+
